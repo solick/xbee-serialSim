4a68b8dd35e1ed3b8653b450ea0bdeae5b91f6dc
solick <matten@lm-consult.com>
2015-01-08 17:43:03 +0100

    initial commit of test nodes

---
+++ nodes.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lynmatten on 08.01.15.
+ */
+
+
+
+var nodeList = [
+
+    {
+        shortMac: "01A1",
+        longMac: "0013A2000000001",
+        LckInterval: 3,
+        data: "AT+1=23.15",
+        type: "Temperature Sensor"
+
+    },
+    {
+        shortMac: "01A2",
+        longMac: "0013A2000000002",
+        LckInterval: 8,
+        data: "AT+1=0",
+        type: "Door Contact Sensor"
+
+    }
+
+];
+
+
+module.exports = nodeList;
