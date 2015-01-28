291c011e9a7c02f1809f6d4c5e286b27c9733198
solick <matten@lm-consult.com>
2015-01-28 12:50:34 +0100

    test now performs a complete simulation for 1.5 min

--- src/test.js
+++ src/test.js
@@ -3,62 +3,40 @@
  */
 
 
-var fs = require('fs');
-
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
 var xbeeAPI = new xbee_api.XBeeAPI();
-
+var zbh = require('xbee-helper');
+var helper = new zbh.ZigBeeHelper();
 
 var SerialSim = require('./serialSim.js');
 var serialSim = new SerialSim.SerialSim();
-
-
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
 
 var nodeList = require('./nodes.js');
 var NodeSim = require('./nodeSim.js');
 var nodeSim = new NodeSim.NodeSim(nodeList);
 
 
-// Something we might receive from an XBee...
-var raw_frame = new Buffer([
-    0x7E, 0x00, 0x13, 0x97, 0x55, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x52, 0x2B,
-    0xAA, 0x7D, 0x84, 0x53, 0x4C, 0x00, 0x40, 0x52, 0x2B, 0xAA, 0xF0
-]);
-
 xbeeAPI.on('data', function(data) {
-
-
-    console.log(">> data received: " + data);
+    //console.log(">> data received: ", data);
 });
 
 // All frames parsed by the XBee will be emitted here
 xbeeAPI.on("frame_object", function(frame) {
-    console.log(">> frame received: ", frame);
-
-    var frameObj = xbeeAPI.parseFrame(frame);
-
-    console.log('>> frame parsed: ', frameObj);
+    //console.log(">> frame received by xbeeAPI: ", frame);
+    console.log(helper.printFrame(frame));
 });
 
-var bla = function bla(data) {
+var emitXbee = function (data) {
 
-    console.log("bla: " + data);
     xbeeAPI.emit('data',data);
     xbeeAPI.emit('frame_object', data);
 };
 
-var sendXbeeFrame = function(node) {
-
-    console.log("sending frame to " + node.shortMac);
-
-};
 
 var displayStart = function() {
 
-    console.log("Event: nodeSim started.");
+    console.log("Event: nodeSim started. -- " + nodeSim.EventCounter() + " Events.");
 
 };
 
@@ -68,81 +46,68 @@ var displayOpen = function() {
 };
 
 
-eventEmitter.on('data', bla);
-nodeSim.on('sendFrame', sendXbeeFrame);
+nodeSim.on('sendFrame', function(rawFrame) {
+
+    serialSim.read(rawFrame);
+
+});
+
+var passFrameToNodeSim = function(data) {
+
+    nodeSim.receiveFrame(data);
+};
+
 nodeSim.on('started', displayStart);
 serialSim.on('open', displayOpen);
 
+serialSim.on('frame_object', emitXbee);
 
-/**
- * check OS
- */
-
-if(process.platform != "darwin")
-{
-    console.log("Your operating system is not supported. Sorry!");
-    process.exit(1);
-}
+serialSim.on('receiveFrame', passFrameToNodeSim);
 
 
-/**
- * check if serial port exists if not create
- * **/
+/******************************************/
 
+serialSim.open(function() {
 
-if(fs.existsSync('/dev/tty.zigbeeVirtual1'))
-{
-    /**
-     * Exists, proceed
-     */
-}
-else
-{
-    /**
-     * Don´t exists, create fifo
-     */
-}
+    console.log("Event: serialSim started.");
 
+    nodeSim.start(function() {
 
-serialSim.open();
+        console.log("Event: nodeSim started. -- " + nodeSim.EventCounter() + " Events.");
+
+    });
+
+    setTimeout(function() {
+
+        var frame_obj = { // AT Request to be sent to
+            type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+            id: 0x01,
+            //destinantion64:"0013A20040ADD0D3",
+            //destination64:"0013A200FFFFFFFF",
+            //destination16: "0x00",
+            remoteCommandOptions: 0x02,
+            command: "MY",
+            commandParameter: [0x01]
+        };
 
 
 
-/*
-eventEmitter.emit('data', raw_frame);
+        serialSim.write(xbeeAPI.buildFrame(frame_obj));
 
-serialSim.write("Test Frame", function(err) {
+    }, 500);
 
-    console.log("with callback!");
-    console.log(err);
+    setTimeout(function() {
+        nodeSim.stop(function() {
+            serialSim.close();
+        });
+
+
+    }, 90000);
+
 
 });
-*/
 
-
-nodeSim.start();
-
-setTimeout(function() {
-    nodeSim.stop();
-    serialSim.close();
-
-}, 30000);
 
 
 
 /**************************************************/
-
-/*
-
- var serialport = new SerialPort("/dev/tty.zbtest1", {
- baudrate: 57600
-
- });
-
- serialport.on("open", function() {
-
- console.log("Serial Port opened.");
-
- });
-
- */
