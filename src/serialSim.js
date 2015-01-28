d63a61ac5d436cc22b2b68569582ab6c5b5a73ba
solick <matten@lm-consult.com>
2015-01-28 12:50:08 +0100

    added test cases. Added code description. read and write function now working.

--- src/serialSim.js
+++ src/serialSim.js
@@ -5,21 +5,41 @@
 
 var events = require('events');
 
-var serialSim = function() {
+
+/***
+ * Class serialSim
+ * simulates a serial line communication for xbee-api
+ * @param path
+ * @param options
+ * @constructor
+ */
+var serialSim = function(path, options) {
 
     events.EventEmitter.call(this);
-
+    this._open = false;
+    this._path = path;
+    this._options = options;
 
 };
 
+/**
+ * prototype for eventemitter
+ * @type {Object|Function|exports.EventEmitter}
+ * @private
+ */
 serialSim.prototype.__proto__ = events.EventEmitter.prototype;
 
 
+/**
+ * function open(), synchron or asynchron
+ * Emitts 'open' event at the end
+ * @param callback
+ */
 serialSim.prototype.open = function(callback) {
 
     var self = this;
 
-    console.log("SerialSim opened.");
+    //console.log("SerialSim opened.");
 
     self.emit('open');
 
@@ -27,11 +47,16 @@ serialSim.prototype.open = function(callback) {
 
 };
 
+/**
+ * function close(), synchron or asynchron
+ * Emitts 'close' event at the end
+ * @param callback
+ */
 serialSim.prototype.close = function(callback) {
 
     var self = this;
 
-    console.log("SerialSim closed.");
+    //console.log("SerialSim closed.");
 
     self.emit('close');
     self.removeAllListeners();
@@ -40,12 +65,24 @@ serialSim.prototype.close = function(callback) {
 
 };
 
+/**
+ * function write(), synchron or asynchron
+ * simulates writing to the serial line
+ * @param data
+ * @param callback
+ */
 serialSim.prototype.write = function(data, callback) {
 
-    //var self = this;
+    var self = this;
+
+    /* TODO: simulate writing to serial Line
+      *  receive a buffer, need to xbeeAPI.parseFrame to get JSON object
+      *  check with addressed node for reaction of command
+       * */
 
     /* write */
-    console.log("writing data: " + data);
+    //console.log("writing data: ", data);
+    self.emit('receiveFrame', data);
 
     if(callback) {
 
@@ -53,6 +90,18 @@ serialSim.prototype.write = function(data, callback) {
 
         callback(err);
     }
+
+};
+
+/**
+ * function read(), synchron
+ * simulates reading event from serial line
+ * Emits 'receivedData' event with data from node
+ * @param rawFrame
+ */
+serialSim.prototype.read = function(rawFrame) {
+
+    this.emit('frame_object', rawFrame);
 
 };
 
--- test/unit/serialSim.spec.js
+++ test/unit/serialSim.spec.js
@@ -1,3 +1,120 @@
 /**
  * Created by lynmatten on 09.01.15.
  */
+
+
+var SerialSim = require('../../src/serialSim.js');
+
+describe('serialSim Class', function() {
+
+    var serialSim = new SerialSim.SerialSim();
+
+    it('should create a valid object', function() {
+
+        expect(serialSim).toBeDefined();
+        expect(serialSim).not.toBeNull();
+
+    });
+
+
+    describe('API', function() {
+
+        var serialSimTest = null;
+
+        beforeEach(function() {
+
+            serialSimTest = new SerialSim.SerialSim();
+
+        });
+
+        afterEach(function() {
+
+
+
+        });
+
+        describe('open() function', function() {
+
+            it('should be implemented', function() {
+
+                expect(serialSimTest.open).toBeDefined();
+
+            });
+
+            it('should throw open event after calling', function(done) {
+
+                serialSimTest.on('open', function() {
+
+                    expect(true).toBeTruthy();
+                    done();
+                });
+
+                serialSimTest.open();
+
+            });
+
+            it('should use the callback method if used', function(done) {
+
+                serialSimTest.open(function() {
+
+                    expect(true).toBeTruthy();
+                    done();
+                });
+
+            });
+
+        });
+
+        describe('close() function', function() {
+
+            it('should be implemented', function() {
+
+                expect(serialSimTest.close).toBeDefined();
+
+            });
+
+            it('should throw close event after calling', function(done) {
+
+                serialSimTest.on('close', function() {
+
+                    expect(true).toBeTruthy();
+                    done();
+                });
+
+                serialSimTest.close();
+
+            });
+
+            it('should use the callback method if used', function(done) {
+
+                serialSimTest.close(function() {
+
+                    expect(true).toBeTruthy();
+                    done();
+                });
+
+            });
+
+        });
+
+        describe('write() function', function() {
+
+            it('should be implemented', function() {
+
+                expect(serialSimTest.write).toBeDefined();
+            });
+
+        });
+
+
+        describe('read() function', function() {
+
+            it('should be implemented', function() {
+
+                expect(serialSimTest.write).toBeDefined();
+            });
+
+        });
+
+    });
+});
