c9a2a61087a3e28fc5ccab63f4a0da80bb7fa2de
solick <matten@lm-consult.com>
2015-03-15 21:36:06 +0100

    added object with sensor objects.

---
+++ src/lmSHObjects.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lynmatten on 16.10.14.
+ */
+
+
+var lmSHObjects = {
+
+    OBJ_LIFECHECK: 1,
+    OBJ_LIFECHECK_INTERVAL: 2,
+    OBJ_TEMP: 3,
+    OBJ_TEMP_INTERVAL: 4,
+    OBJ_DOOR_CONTACT: 5,
+    OBJ_MOVEMENT_DETECTOR: 6,
+    OBJ_RFID:7
+
+
+
+};
+
+module.exports = lmSHObjects;
+
